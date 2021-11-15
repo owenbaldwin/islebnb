1d07f586a5eff38c3ee25498ee0032bb6771c8e0
Owen Baldwin <owenabaldwin@gmail.com>
2021-11-15 15:47:01 +0000

    devise done and working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :islands do
+    resources :bookings, only: [:new, :create]
+  end
+  # resources :users
 end
