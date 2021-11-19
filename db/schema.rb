de8a7d76c389acd62fd5f43832b0e90c062c51a7
Zoe Bell <zoecharlottebell@gmail.com>
2021-11-19 15:34:49 +0000

    new island not booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_160502) do
+ActiveRecord::Schema.define(version: 2021_11_19_124819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,12 @@ ActiveRecord::Schema.define(version: 2021_11_18_160502) do
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_islands_on_user_id"
+  end
+
+  create_table "secondary_photos", force: :cascade do |t|
+    t.string "link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
